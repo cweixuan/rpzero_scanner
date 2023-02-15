0cbf39606a963c29077d0c1577abbee53024f28b
cweixuan <cweixuan1999@gmail.com>
2023-02-15 22:48:15 +0800

    build for rpi

---
+++ build/examples/nordic_uart/CMakeFiles/nordic_uart.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/pi/Documents/rpzero_scanner/examples/nordic_uart/nordic_uart.c" "/home/pi/Documents/rpzero_scanner/build/examples/nordic_uart/CMakeFiles/nordic_uart.dir/nordic_uart.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "BLUEZ_VERSION_MAJOR=5"
+  "BLUEZ_VERSION_MINOR=43"
+  "GATTLIB_LOG_BACKEND_SYSLOG"
+  "GATTLIB_LOG_LEVEL=3"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  "../dbus/../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/pi/Documents/rpzero_scanner/build/dbus/CMakeFiles/gattlib.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
