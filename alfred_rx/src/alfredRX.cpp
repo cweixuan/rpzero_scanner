5c5d1c5262828efcc94d2b5aa6b305651c9b8e70
unknown <cweixuan1999@gmail.com>
2023-09-12 17:03:38 +0800

    added while loop with 1s delay in between

--- alfred_rx/src/alfredRX.cpp
+++ alfred_rx/src/alfredRX.cpp
@@ -9,7 +9,11 @@ int main(){
     char rx_buf[MAX_PAYLOAD];
     int rx_len = 0;
     auto redis = sw::redis::Redis("tcp://127.0.0.1:6379");
-    alfred_req_data_redis(65,redis);
+    while(1){
+        time_t last_time = std::chrono::system_clock::now();
+        alfred_req_data_redis(65,redis);
+        std::this_thread::sleep_until(last_time+ std::chrono::seconds(1));
+    }
     int curr_len = 0;
     int num_points;
     
